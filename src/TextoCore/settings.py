d39f8a3391e437728e915187db55d8ed34ad502e
Hmax <hmxyz@outlook.com>
2019-11-05 15:11:34 +0100

    staticfiles

--- src/TextoCore/settings.py
+++ src/TextoCore/settings.py
@@ -132,15 +132,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, "assets")
 ]
 
 
