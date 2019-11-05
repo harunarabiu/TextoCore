6e0702e120ab282cc3956f1c8e4165ceb96a7de0
Hmax <hmxyz@outlook.com>
2019-11-05 13:59:09 +0100

    static root

--- src/TextoCore/settings.py
+++ src/TextoCore/settings.py
@@ -132,15 +132,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets')
-]
 
 
 AUTH_USER_MODEL = 'account.User'
